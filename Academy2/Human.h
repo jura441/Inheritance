56ae9fa3b76258b5b3df83a2f11c3a36a129c211
jura441 <102172272+jura441@users.noreply.github.com>
2022-06-10 19:19:34 +0300

    Academy Separated

---
+++ Academy2/Human.h
@@ -0,0 +1,58 @@
+#pragma once
+#include "std.h"
+
+
+#define HUMAN_TAKE_PARAMETERS const std::string last_name, const std::string& first_name, unsigned int age
+#define HUMAN_GIVE_PARAMETERS last_name, first_name, age
+
+class Human
+{
+protected:
+	std::string last_name;
+	std::string first_name;
+	unsigned int age;
+public:
+	const std::string& get_last_name()const
+	{
+		return last_name;
+	}
+	const std::string& get_first_name()const
+	{
+		return first_name;
+	}
+	unsigned int get_age()const
+	{
+		return age;
+	}
+	void set_last_name(const std::string& last_name)
+	{
+		this->last_name = last_name;
+	}
+	void set_first_name(const std::string& first_name)
+	{
+		this->first_name = first_name;
+	}
+	void set_age(unsigned int age)
+	{
+		this->age = age;
+	}
+
+	//					Constructors:
+	Human(HUMAN_TAKE_PARAMETERS)
+	{
+		set_last_name(last_name);
+		set_first_name(first_name);
+		set_age(age);
+		cout << "HConstructor:\t" << this << endl;
+	}
+	~Human()
+	{
+		cout << "HDestructor:\t" << this << endl;
+	}
+
+	//					Methods:
+	virtual void print()const
+	{
+		cout << last_name << " " << first_name << " " << age << " years.\n";
+	}
+};
---
+++ Academy2/std.h
@@ -0,0 +1,7 @@
+#pragma once
+#include<iostream>
+#include<string>
+using std::cin;
+using std::cout;
+using std::endl;
+
