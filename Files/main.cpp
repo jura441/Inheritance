d8b655c3324abd2eb984c8ccd3652c8a1b5d0e49
jura441 <102172272+jura441@users.noreply.github.com>
2022-06-13 22:04:05 +0300

    Files 1

---
+++ Files/main.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+#include<fstream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	std::ofstream fout;							//1) создаем поток
+	fout.open("File.txt", std::ios_base::app);	//2) открываем поток
+	fout << "Hello World!\n";					//3) выводим поток
+	fout.close();								//4) закрываем поток
+	// Потоки обязательно нужно закрывать, это так же важно, как удалять память.
+	//Сколько раз был вызван метов open(), столько же раз нужно вызвать метод close();
+	system("notepad File.txt");
+}
